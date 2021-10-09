0aeb83d32d1603723489f656a59e5e5dd851bdc1
prateek565 <sainiprateek565@gmail.com>
2021-10-09 10:52:55 +0530

    dice wth img

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -19,14 +19,28 @@ class MainActivity : AppCompatActivity() {
 //            resultTextView.text ="6"
             rollDice()
         }
+      rollDice()    // do  a dice roll  when app stars
     }
 
     // roll the dice and update screen with result
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = "$diceRoll"
+
+//        find img view in layut
+        val diceImage :ImageView =findViewById(R.id.imageView)
+
+//        determines which resource id to use
+        when(diceRoll){
+            1->diceImage.setImageResource(R.drawable.dice_1)
+            2->diceImage.setImageResource(R.drawable.dice_2)
+            3->diceImage.setImageResource(R.drawable.dice_3)
+            4->diceImage.setImageResource(R.drawable.dice_4)
+            5->diceImage.setImageResource(R.drawable.dice_5)
+            6->diceImage.setImageResource(R.drawable.dice_6)
+            }
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
 //create a dice and roll it random no give
